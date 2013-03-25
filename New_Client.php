b34506d045387783596501e59382787e5eb6e143
thomasmccaffery <tom@thomasmccaffery.com>
2013-03-24 23:47:16 -0300

    Create New_Client.php

---
+++ New_Client.php
@@ -0,0 +1,16 @@
+
+<form action="Add_Client.php" method="POST">
+<p>Twitter Username:</p> <input type="text" name="tid">
+<p>Facebook ID:</p> <input type="text" name="fbid">
+<p>Progress</p> <select name="progress">
+  <option value="0">Not Started</option>
+  <option value="1">In Progress</option>
+  <option value="2">Finishing Up</option>
+  <option value="3">Finished</option>
+  <option value="4">Christine</option>
+  <option value="5">Christine-Done</option>
+</select> 
+<p>Genesis PW:</p> <input type="text" name="genPW">
+<br />
+<input type="submit" value="Send"><input type="reset" value="Clear">
+</form>
